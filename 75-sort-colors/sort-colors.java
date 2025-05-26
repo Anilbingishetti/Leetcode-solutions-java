9aecdf29cdaa47223e75d5bb56b954fca8751f77
Anil Bingishetti <120564456+Anilbingishetti@users.noreply.github.com>
2025-05-26 08:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (92.54%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,31 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        quicksort(nums,0,nums.length-1);
+    }
+    public void quicksort(int []arr, int low, int high){
+        if(low < high)
+        {
+            int pi = part(arr,low,high);
+            quicksort(arr,low,pi-1);
+            quicksort(arr,pi+1,high);
+        }
+    }
+    private int part(int []arr, int low, int high){
+        int pi = arr[high];
+        int i = low-1;
+        for(int j=low;j<high;j++)
+        {
+           if(arr[j] < pi)
+           {
+               i++;
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+           }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
     }
 }
