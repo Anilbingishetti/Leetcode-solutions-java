e598baf2faa8f815c8accda5d5537599d82863c9
Anil Bingishetti <120564456+Anilbingishetti@users.noreply.github.com>
2025-07-26 11:21:17 +0530

    Time: 45 ms (5.40%) | Memory: 55.8 MB (88.57%) - LeetSync

---
+++ 1945-finding-the-users-active-minutes/finding-the-users-active-minutes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        int [] ans = new int [k];
+        HashMap<Integer,Set<Integer>> map = new HashMap<>();
+        for(int i=0;i<logs.length;i++)
+        {
+            int id = logs[i][0];
+            int ti = logs[i][1];
+            if(!map.containsKey(id)){
+            map.put(id,new  HashSet<Integer> ());
+            map.get(id).add(ti);
+            }
+            else{
+                map.get(id).add(ti);
+            }
+        }
+        System.out.print(map);
+        for(int i : map.keySet()){
+            Set<Integer> set = map.get(i);
+            int s = set.size();
+            ans[s-1]++;
+        }
+        return ans;
+    }
+}
