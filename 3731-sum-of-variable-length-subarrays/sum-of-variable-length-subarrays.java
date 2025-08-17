e35f3ae9fbe94e83a889f09ebc168e19d13349bc
Anil Bingishetti <120564456+Anilbingishetti@users.noreply.github.com>
2025-08-17 18:51:48 +0530

    Time: 1 ms (98.78%) | Memory: 43.3 MB (85.73%) - LeetSync

--- 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.java
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.java
@@ -3,8 +3,7 @@ class Solution {
         int sum = 0;
         for(int i=0;i<nums.length;i++)
         {
-            int left = Math.max(0,i-nums[i]);
-            for(int j=left;j<=i;j++)
+            for(int j=Math.max(0,i-nums[i]);j<=i;j++)
             {
                 sum+=nums[j];
             }
