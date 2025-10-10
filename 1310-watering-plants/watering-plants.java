769f9d04dc4f7c6a3a9cb37f4ca90035d3fc109b
Anil Bingishetti <120564456+Anilbingishetti@users.noreply.github.com>
2025-10-10 16:13:32 +0530

    Time: 1 ms (28.93%) | Memory: 42.8 MB (17.46%) - LeetSync

--- 1310-watering-plants/watering-plants.java
+++ 1310-watering-plants/watering-plants.java
@@ -2,12 +2,18 @@ class Solution {
     public int wateringPlants(int[] plants, int capacity) {
         int ans = 0;
         int carry = capacity;
+        boolean went_back = false;
         for(int i=0;i<plants.length;i++)
         {
             if(plants[i] > carry)
             {
-                ans+= 2*i;
+                ans+=i;
+                went_back = true;
                 carry = capacity;
+            }
+            if(went_back){
+                ans+=i;
+                went_back = false;
             }
             ans+=1;
             carry-=plants[i];
