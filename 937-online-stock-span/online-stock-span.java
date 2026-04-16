84e11035ac74a388421c365610b63d9130c3f799
Anil Bingishetti <120564456+Anilbingishetti@users.noreply.github.com>
2026-04-16 09:16:04 +0530

    Time: 1157 ms (5.01%) | Memory: 51.9 MB (93.89%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,30 @@
+class StockSpanner {
+    List<Integer> li;
+    public StockSpanner() {
+        this.li = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        li.add(price);
+        int count = 1;
+        if(li.size() > 1){
+        for(int i=li.size()-2;i>=0;i--)
+        {
+            if(li.get(i) <= price)
+            {
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        }
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
